415143ed2cc3dad4d5bb898f17531fad7b97757c
Gabriel Tellaroli Ramos <bi.tellaroli1@hotmail.com>
2021-06-24 10:02:34 -0300

    refactor(index.js): :bug: Removi a estrutura forEach, e separei as funções.

--- index.js
+++ index.js
@@ -33,119 +33,104 @@ if (process.argv.slice(2)[1] === 'xlsx') {
 let filename = `${__dirname}/results/results_${new Date().getTime()}.${export_opt}`;
 
 const configFile = require(`./config/${config_file}`);
-configFile.validator.forEach(async (config) => {
-  const schema = require(`./schema/${config.schema_name[0]}`);
-  // let startChromeInDebugMode = async () => {
-  //   let operatingSystem = os.platform();
-  //   if (operatingSystem === "linux") {
-  //     await exec(
-  //       "google-chrome --remote-debugging-port=9222 --user-data-dir=remote-profile"
-  //     );
-  //   } else if (operatingSystem === "win32") {
-  //      await exec(
-  //       "start chrome --remote-debugging-port=9222 --user-data-dir=remote-profile"
-  //     );
-  //   }
 
-  //   return new Promise((res) => {
-  //     setTimeout((_) => {
-  //       req("http://localhost:9222/json/version")
-  //         .then((result) => {
-  //           res(JSON.parse(result).webSocketDebuggerUrl);
-  //         })
-  //         .catch((err) =>
-  //           console.log(
-  //             "Error while trying to pick the Debugger URL. Did you open the chrome instance with debugging port?"
-  //           )
-  //         );
-  //     }, 5000);
-  //   });
-  // };
+async function runAfterGTMDebug(config, page, browser, filename) {
+  page.on('load', async () => {
+    await page.waitFor(2000);
+    if (page.url() === config.url) {
+      //doc.text(path, 10, 10);
+      await page.evaluate(() => {
+        //Validate first hits.
+        window.dataLayer.forEach((elem) => {
+          bowser(elem);
+        });
+        window.dataLayer.push_c = window.dataLayer.push;
+        window.dataLayer.push = function (obj) {
+          window.dataLayer.push_c(obj);
+          bowser(obj);
+        };
+      });
 
-  (async () => {
-    // const browser = await puppeteer.connect({
-    //   browserWSEndpoint: await startChromeInDebugMode(),
-    // });
-    const browser = await puppeteer.launch({ headless: false });
-    let page = await browser.newPage();
-
-    // .pages
-    // .then((res) => res[0])
-    // .catch((err) => new Error(err));
-
-    if (config.gtmPreviewModeURL) await page.goto(config.gtmPreviewModeURL);
-
-    if (config.mobile_enabled === true) {
-      await page.emulate(iPhone);
+      if (config.browserClose) {
+        config.time ? await page.waitFor(config.time) : await page.waitFor(0);
+        await browser.close();
+      }
     } else {
-      await page.setViewport({ width: 1366, height: 768 });
-    }
-
-    await page.exposeFunction('bowser', (event) => {
-      bowserjr.validate(schema, event, function (result) {
-        resultsArray.push(result[0]);
+      await fs.appendFileSync(filename, `Path :  ${page.url()}\n`, (err) => {
+        if (err) throw err;
       });
+      //doc.text(path, 10, 10);
+    }
+  });
+
+  await page.goto(config.url);
+}
+
+async function browserCrawler(config) {
+  const schema = require(`./schema/${config.schema_name[0]}`);
+
+  const browser = await puppeteer.launch({ headless: false });
+  let page = await browser.newPage();
+
+  if (config.gtmPreviewModeURL) await page.goto(config.gtmPreviewModeURL);
+
+  if (config.mobile_enabled === true) {
+    await page.emulate(iPhone);
+  } else {
+    await page.setViewport({ width: 1366, height: 768 });
+  }
+
+  await page.exposeFunction('bowser', (event) => {
+    bowserjr.validate(schema, event, function (result) {
+      resultsArray.push(result[0]);
     });
+  });
 
-    async function runAfterGTMDebug() {
-      page.on('load', async () => {
-        await page.waitFor(2000);
-        if (page.url() === config.url) {
-          await fs.appendFileSync(filename, `Url validating:  ${page.url()}\n`, (err) => {
+  await runAfterGTMDebug(config, page, browser, filename);
+
+  await fs.appendFile(filename, '', () => {});
+
+  let cleanupEval = () => {
+    console.log('Realizing last eval');
+    bowserjr.validate(schema, {}, function (result) {
+      resultsArray.push(result[0]);
+    });
+    fs.appendFileSync(filename, `Url validating:  ${page.url()}\n`, (err) => {
+      if (err) throw err;
+    });
+    resultsArray.forEach((resultObject) => {
+      if (resultObject) {
+        resultObject.dataLayerObject = resultObject.dataLayerObject.replace(/(\r\n|\n|\r)/gm, '');
+        resultObject.dataLayerObject = resultObject.dataLayerObject.replace(/\s/g, '');
+        resultObject.dataLayerObject = resultObject.dataLayerObject.split(',').join(' ');
+        fs.appendFileSync(
+          filename,
+          `${resultObject.status}, ${resultObject.message}, ${resultObject.dataLayerObject}\n`,
+          (err) => {
             if (err) throw err;
-          });
-          //doc.text(path, 10, 10);
-          await page.evaluate(() => {
-            //Validate first hits.
-            window.dataLayer.forEach((elem) => {
-              bowser(elem);
-            });
-            window.dataLayer.push_c = window.dataLayer.push;
-            window.dataLayer.push = function (obj) {
-              window.dataLayer.push_c(obj);
-              bowser(obj);
-            };
-          });
-          if (config.browserClose) {
-            config.time ? await page.waitFor(config.time) : await page.waitFor(0);
-            await browser.close();
           }
-        } else {
-          await fs.appendFileSync(filename, `Path :  ${page.url()}\n`, (err) => {
-            if (err) throw err;
-          });
-          //doc.text(path, 10, 10);
-        }
-      });
+        );
+      }
+    });
+    fs.appendFileSync(filename, `\n`, (err) => {
+      if (err) throw err;
+    });
+  };
 
-      await page.goto(config.url);
-    }
+  await process.on('exit', cleanupEval);
+}
 
-    await runAfterGTMDebug();
+async function teste() {
+  const config = configFile.validator[0];
 
-    await fs.appendFile(filename, '', () => {});
+  console.log('Começou 1 execução...');
+  await browserCrawler(config);
+  console.log('Terminou 1 execução.');
 
-    let cleanupEval = () => {
-      console.log('Realizing last eval');
-      bowserjr.validate(schema, {}, function (result) {
-        resultsArray.push(result[0]);
-      });
-      resultsArray.forEach((resultObject) => {
-        if (resultObject) {
-          resultObject.dataLayerObject = resultObject.dataLayerObject.replace(/(\r\n|\n|\r)/gm, '');
-          resultObject.dataLayerObject = resultObject.dataLayerObject.replace(/\s/g, '');
-          resultObject.dataLayerObject = resultObject.dataLayerObject.split(',').join(' ');
-          fs.appendFileSync(
-            filename,
-            `${resultObject.status}, ${resultObject.message}, ${resultObject.dataLayerObject}\n`,
-            (err) => {
-              if (err) throw err;
-            }
-          );
-        }
-      });
-    };
+  const config2 = configFile.validator[1];
 
-    await process.on('exit', cleanupEval);
-  })();
-});
+  console.log('Começou 2 execução...');
+  await browserCrawler(config2);
+  console.log('Terminou 2 execução.');
+}
+teste();
